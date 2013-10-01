bbf8fbe1af86be8689ff5b0056ba8b66007a262c
Damien Krotkine <dkrotkine@weborama.com>
2013-10-01 14:46:53 +0200

    fix default sysread / syswrite when an error happens

--- lib/PerlIO/via/Timeout/Strategy/NoTimeout.pm
+++ lib/PerlIO/via/Timeout/Strategy/NoTimeout.pm
@@ -21,12 +21,23 @@ our @ISA = qw(PerlIO::via::Timeout::Strategy);
 
 sub READ {
     my ($self, undef, $len, $fh, $fd) = @_;
-    return sysread($fh, $_[1], $len);
+    my $rv = sysread($fh, $_[1], $len);
+    if (! defined $rv) {
+        # There is a bug in PerlIO::via (possibly in PerlIO ?). We would like
+        # to return -1 to signify error, but doing so doesn't work (it usually
+        # segfault), it looks like the implementation is not complete. So we
+        # return 0.
+        $rv = 0;
+    }
+    return $rv;
 }
 
 sub WRITE {
     my ($self, undef, $fh, $fd) = @_;
-    return syswrite($fh, $_[1]);
+    my $rv syswrite($fh, $_[1]);
+    defined $rv
+      or return -1;
+    return $rv;
 }
 
 1;
