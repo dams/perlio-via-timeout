bdddf43484741f0232369493eb7b24aad3b48035
Damien Krotkine <dkrotkine@booking.com>
2015-05-13 20:13:17 +0200

    cleanup, fix docs, add has_timeout_layer to safely check if a fh is handled

--- lib/PerlIO/via/Timeout.pm
+++ lib/PerlIO/via/Timeout.pm
@@ -3,11 +3,11 @@ package PerlIO::via::Timeout;
 # ABSTRACT: a PerlIO layer that adds read & write timeout to a handle
 
 require 5.008;
+use Time::HiRes;
 use strict;
 use warnings;
 use Carp;
 use Errno qw(EBADF EINTR ETIMEDOUT);
-use Scalar::Util qw(reftype blessed weaken);
 
 use Exporter 'import'; # gives you Exporter's import() method directly
 
@@ -25,7 +25,7 @@ The timeout is implemented by using C<<select>> on the handle before
 reading/writing.
 
 B<WARNING> the handle won't timeout if you use C<sysread> or C<syswrite> on it,
-because these functions works at a lower level. Hower if you're trying to
+because these functions works at a lower level. However if you're trying to
 implement a timeout for a socket, see L<IO::Socket::Timeout> that implements
 exactly that.
 
@@ -150,7 +150,7 @@ sub WRITE {
 sub _can_read_write {
     my ($fh, $fd, $timeout, $type) = @_;
     # $type: 0 = read, 1 = write
-    my $initial = time;
+    my $initial = Time::HiRes::time;
     my $pending = $timeout;
     my $nfound;
 
@@ -167,7 +167,7 @@ sub _can_read_write {
         if ($nfound == -1) {
             $! == EINTR
               or croak(qq/select(2): '$!'/);
-            redo if !$timeout || ($pending = $timeout - (time -
+            redo if !$timeout || ($pending = $timeout - (Time::HiRes::time -
             $initial)) > 0;
             $nfound = 0;
         }
@@ -197,9 +197,9 @@ sub read_timeout {
 =func write_timeout
 
   # set a write timeout of 2.5 seconds
-  timeout_layer($fh)->write_timeout(2.5);
+  write_timeout($fh, 2.5);
   # get the current write timeout
-  my $secs = timeout_layer($fh)->write_timeout();
+  my $secs = write_timeout($fh);
 
 Getter / setter of the write timeout value.
 
@@ -229,7 +229,7 @@ sub enable_timeout { timeout_enabled($_[0], 1) }
 
 =func disable_timeout
 
-  timeout_layer($fh)->disable_timeout();
+  disable_timeout($fh);
 
 Equivalent to setting timeout_enabled to 0
 
@@ -254,6 +254,23 @@ sub timeout_enabled {
     my $prop = __PACKAGE__->_fh2prop($_[0]);
     @_ > 1 and $prop->{timeout_enabled} = !!$_[1];
     $prop->{timeout_enabled};
+}
+
+=func has_timeout_layer
+
+  if (has_timeout_layer($fh)) {
+    # set a write timeout of 2.5 seconds
+    write_timeout($fh, 2.5);
+  }
+
+Returns wether the given filehandle is managed by PerlIO::via::Timeout.
+
+=cut
+
+sub has_timeout_layer {
+    defined (my $fd = _get_fd($_[0]))
+      or return;
+    exists $fd2prop{$fd};
 }
 
 1;
